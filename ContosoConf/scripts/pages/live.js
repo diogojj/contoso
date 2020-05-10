be4aad627f705f258f3cf26599005786ad6e4f70
diogo justino <diogo.av.justino@gmail.com>
2020-05-11 00:46:01 +0100

    test4

--- ContosoConf/scripts/pages/live.js
+++ ContosoConf/scripts/pages/live.js
@@ -1,6 +1,6 @@
 ﻿import { LivePage } from "../LivePage.js";
 // TODO: Create a web socket connection to ws://localhost:55981/live/socket.ashx
-const socket = new WebSocket("ws://diogojjcontoso.azurewebsites.net//live/socket.ashx");
+const socket = new WebSocket("wss://diogojjcontoso.azurewebsites.net//live/socket.ashx");
 new LivePage(
     socket,
     document.querySelector("section.live")
